e0ee180be4f7cef277b4be6075e57fb5f555c6ec
Daan <20974756+daanschenkel@users.noreply.github.com>
2023-11-22 14:54:21 +0100

    Redirect page

--- src/routes/format/[format]/json/+server.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import rootPathLib from 'app-root-path';
-const { resolve } = rootPathLib;
-import { json } from '@sveltejs/kit';
-export function GET({ params }) {
-	const data = fs.readFileSync(resolve(`/static/formats/${params.format}.json`), 'utf8');
-	const format = JSON.parse(data);
-	return json({ format });
-}
